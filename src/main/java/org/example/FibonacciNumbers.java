54b09d87e9b87bf12e9ef8fa90d5650e8220161e
KovGr <KovGrDm@outlook.com>
2024-05-06 21:28:23 +0300

    HW5

--- src/main/java/org/example/FibonacciNumbers.java
+++ src/main/java/org/example/FibonacciNumbers.java
@@ -5,7 +5,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Main {
+public class FibonacciNumbers {
+   public static List<Long> fibonacciNumbers = new ArrayList<>();
+   public static  Map<Integer, Long> fibonacciNumbersDP = new HashMap<>();
+
     public static void main(String[] args) {
 
         System.out.println("Iterative = " + fibonacciIterative(6));
@@ -13,29 +16,38 @@ public class Main {
         System.out.println("Recursive = " + fibonacciRecursive(6));
 
         System.out.println("DP = " + fibonacciDP(6));
+
     }
 
     private static long fibonacciIterative(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("Input should be >= 0");
         }
-        List<Long> fibonacciNumbers = new ArrayList<>();
-        if (n <= 1) {
+        if (n > 92) {
+            throw new IllegalArgumentException("Input should be <= 92");
+        }
+        if (n == 0) {
+            return 0;
+        }
+        if (n <= 2) {
             return 1L;
         }
         fibonacciNumbers.add(1L);
         fibonacciNumbers.add(1L);
 
         for (int i = 2; i <= n; i++) {
+
             fibonacciNumbers.add(fibonacciNumbers.get(i - 1) + fibonacciNumbers.get(i - 2));
         }
-
         return fibonacciNumbers.get(n-1);
     }
 
     private static long fibonacciRecursive(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("Input should be >= 0");
+        }
+        if (n > 92) {
+            throw new IllegalArgumentException("Input should be <= 92");
         }
         if (n <= 1) {
             return n;
@@ -47,18 +59,17 @@ public class Main {
         if (n < 0) {
             throw new IllegalArgumentException("Input should be >= 0");
         }
-        Map<Integer, Long> fibonacciNumbers = new HashMap<>();
-
+        if (n > 92) {
+            throw new IllegalArgumentException("Input should be <= 92");
+        }
         if (n <= 1) {
             return n;
         }
-
-        if (fibonacciNumbers.containsKey(n)) {
-            return fibonacciNumbers.get(n);
+        if (fibonacciNumbersDP.containsKey(n)) {
+            return fibonacciNumbersDP.get(n);
         }
-
         long fibonacciNumber = fibonacciDP(n - 1) + fibonacciDP(n - 2);
-        fibonacciNumbers.put(n, fibonacciNumber);
+        fibonacciNumbersDP.put(n, fibonacciNumber);
         return fibonacciNumber;
     }
 }
